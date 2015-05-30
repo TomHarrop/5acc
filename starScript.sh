e1e61e4532009b543ce4598768dddb4a2bf85f4b
Tom Harrop <twharrop@gmail.com>
2015-05-30 09:34:47 +0200

    files for running jobs on cluster; need to consolidate

---
+++ starScript.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+############      SGE CONFIGURATION      ###################
+# join error 
+#$ -j y
+
+# Which shell
+#$ -S /bin/bash 
+
+# Email pour suivre l'execution 
+#$ -M thomas.harrop@ird.fr
+
+# Type de massage que l'on reçoit par mail
+#    -  (b) un message au demarrage
+#    -  (e) a la fin
+#    -  (a)  en cas d'abandon
+#$ -m bea 
+
+# Queue que l'on veut utiliser
+#$ -q highmem.q
+
+# Nom du job
+#$ -N star
+
+# Resources
+#$ -pe smp 4
+
+############################################################
+ 
+path_to_dir="/data/projects/evoreprice/Tom"
+path_to_tmp="/scratch/tom-$JOB_ID"
+path_to_out="/scratch/tom-$JOB_ID-out"
+
+###### Catch arguments
+
+echo -e "\n[ `date`: Preparing to run STAR for library $library_name ] :\nfwd_read_file=$fwd_read_file\nrev_read_file=$rev_read_file\n"
+fwd_basename=$(basename $fwd_read_file)
+rev_basename=$(basename $rev_read_file)
+R1=$path_to_tmp/$fwd_basename
+R2=$path_to_tmp/$rev_basename
+index_dir=/data/projects/evoreprice/Tom/star/index
+
+###### Create temporary folders on node
+mkdir -p $path_to_tmp
+mkdir -p $path_to_out/$library_name
+echo -e "\n[ `date`: Data transfer master -> node ]\n"
+scp -rp $fwd_read_file $R1
+scp -rp $rev_read_file $R2
+
+ls -la $path_to_tmp/
+
+###### Run STAR
+
+gff=$path_to_tmp/genome/Osativa_204_v7.0.gene_exons.rRNAremoved.gff3
+fasta=$path_to_tmp/genome/Osativa_204_v7.0.fa
+
+# ENCODE options
+# cmd="/usr/local/STAR-2.4.1.c/source/STAR --runThreadN 4 --genomeDir $index_dir --readFilesIn $R1 $R2 --outFilterType BySJout --outFilterMultimapNmax 20 --alignSJoverhangMin 8 --alignSJDBoverhangMin 1 --outFilterMismatchNmax 999 --outFilterMismatchNoverLmax 0.04 --alignIntronMin 20 --alignIntronMax 1000000 --alignMatesGapMax 1000000 --outFileNamePrefix $path_to_out/$library_name. --outSAMtype BAM Unsorted --outSJfilterReads Unique --outSJfilterCountUniqueMin 5 5 5 5 --outSJfilterCountTotalMin 5 5 5 5 --outSJfilterIntronMaxVsReadN 5000"
+# default options
+cmd="/usr/local/STAR-2.4.1.c/source/STAR --runThreadN 4 --genomeDir $index_dir --readFilesIn $R1 $R2 --outFileNamePrefix $path_to_out/$library_name. --outSAMtype BAM Unsorted"
+
+echo -e "\n[ `date`: Running command ]\n$cmd\n"
+$cmd
+
+##### Transfer results
+echo -e "\n[ `date`: Results transfer node -> master ]\n"
+rcp -rp $path_to_out $path_to_dir
+
+#### Suppression du repertoire tmp noeud
+echo -e "\n[ `date`: Deleting temporary files on node ]\n"
+rm -rf $path_to_tmp
+rm -rf $path_to_out
---
+++ submitStarJobs.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+for fwd_read_file in $(ls /data/projects/evoreprice/Data/O*/Trimmed/*-cutadapt_cluster-20150522/*R1*fastq)
+do
+	n=$(basename $fwd_read_file)
+	library_name=${n:0:2}
+	rev_read_file=$(ls ${fwd_read_file/$library_name*R1/$library_name*R2})
+	echo $library_name
+	echo "R1: "
+	echo $fwd_read_file
+	echo "R2: "
+	echo $rev_read_file
+	echo "qsub -v fwd_read_file=$fwd_read_file,rev_read_file=$rev_read_file,library_name=$library_name /data/projects/evoreprice/Tom/scripts/starScript.sh"
+done
