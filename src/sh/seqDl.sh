e1581457c444759f9452a53ad1031ded7695b481
Tom Harrop <twharrop@gmail.com>
2015-06-23 15:29:32 +0200

    cutadapt via SBATCH. nice

--- src/sh/seqDl.sh
+++ src/sh/seqDl.sh
@@ -2,66 +2,66 @@
 
 cd data/reads
 
-# srun -c 1 -J dl -o /dev/null curl -O ***REMOVED*** &
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
-# srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O ***REMOVED*** &
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
+srun -c 1 -J dl -o /dev/null curl -O***REMOVED***
 
 echo -e "[ Script ]\t${0}" > METADATA.tsv
 exit 0
