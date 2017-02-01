f8b024f3873338e7875801620a48034f3ee3948c
Tom Harrop <twharrop@gmail.com>
2017-02-01 14:27:55 +1300

    mistake selecting significant genes

--- src/R/genes_under_selection_5acc.R
+++ src/R/genes_under_selection_5acc.R
@@ -7,11 +7,11 @@ gene.p.matrix <- readRDS("output/gene_p_matrix/gene_p_matrix.RDS")
 annotated.window.genes <- readRDS(
   "output/genes_under_selection/annotated_window_genes.RDS")
 
+gene.p.matrix[, symbols := NULL]
 p.value.columns <- names(gene.p.matrix)[
   !names(gene.p.matrix) %in% c("gene", "crowell_qtl", "lmi_qtl")]
 
-# merge
-gene.p.matrix[, symbols := NULL]
+# mergeselection.with.p
 setkey(gene.p.matrix, gene)
 setkey(annotated.window.genes, gene)
 selection.with.p <- gene.p.matrix[annotated.window.genes]
@@ -25,10 +25,22 @@ setcolorder(selection.with.p, c(
 any.sig <- selection.with.p[!is.na(domestication),
                             .(any.sig = any(.SD < 0.1)),
                             by = gene,
-                            .SDcols = p.value.columns][, unique(gene)]
+                            .SDcols = p.value.columns][any.sig == TRUE,
+                                                       unique(gene)]
 sig.in.selection <- selection.with.p[gene %in% any.sig |
                                        crowell_qtl == TRUE |
                                        lmi_qtl == TRUE]
+
+# are there any domestication genes in the selection windows?
+dom.cols <- grep("^domestication", p.value.columns, value = TRUE)
+tested.genes <- selection.with.p[, !all(is.na(.SD)),
+                                 .SDcols = dom.cols,
+                                 by = gene][V1 == TRUE, unique(gene)]
+dom.genes <- selection.with.p[gene %in% tested.genes,
+                              any(.SD < 0.1),
+                              .SDcols = dom.cols,
+                              by = gene][V1 == TRUE, unique(gene)]
+dom.in.selection <- selection.with.p[gene %in% dom.genes]
 
 # write output
 outdir = "output/genes_under_selection"
@@ -44,6 +56,12 @@ write.table(selection.with.p,
             row.names = FALSE)
 write.table(sig.in.selection,
             paste0(outdir, "/window_genes_x_5acc_sig.tab"),
+            sep = "\t",
+            quote = FALSE,
+            na = "",
+            row.names = FALSE)
+write.table(dom.in.selection,
+            paste0(outdir, "/window_genes_x_5acc_sig_dom.tab"),
             sep = "\t",
             quote = FALSE,
             na = "",
