cd63f8cace4832eb5625d46bc3051eb080e1cd03
Tom Harrop <twharrop@gmail.com>
2018-07-30 09:32:34 +1200

    draft PCA plot

---
+++ src/pca_plots.R
@@ -0,0 +1,100 @@
+library(data.table)
+library(ggplot2)
+library(grid)
+library(gridExtra)
+
+pca_file <- "output/080_phenotype/cali_pca.Rds"
+pheno_file <- "output/080_phenotype/cali.csv"
+
+########
+# MAIN #
+########
+
+# read data
+pca <- readRDS(pca_file)
+pheno <- fread(pheno_file)
+
+# make plotting data
+spec_order <- c("rufipogon",
+                "indica",
+                "japonica",
+                "sativa",
+                "barthii",
+                "glaberrima")
+
+pca_pheno <- cbind(pheno, pca$x)
+pca_pd <- melt(pca_pheno,
+               id.vars = c("Species",
+                           "Sowing_nb",
+                           "Repet_nb",
+                           "Plant_nb",
+                           "Panicle_nb"),
+               measure.vars = paste0("PC", 1:4),
+               variable.name = "component")
+pca_pd[, Species := factor(Species, levels = spec_order)]
+
+# loadings
+loadings_wide <- data.table(pca$rotation, keep.rownames = TRUE)
+setnames(loadings_wide, "rn", "phenotype")
+
+# cluster loadings for plot order
+hc <- hclust(dist(pca$rotation, method = "minkowski"),
+       method = "ward")
+pheno_order <- hc$labels[hc$order]
+
+# generate loadings plot data
+loadings_pd <- melt(loadings_wide,
+     id.vars = "phenotype",
+     measure.vars = paste0("PC", 1:4),
+     variable.name = "component")
+loadings_pd[, phenotype := factor(phenotype, levels = pheno_order)]
+
+# plot pca
+pd <- RColorBrewer::brewer.pal(4, "Paired")
+
+pcp <- ggplot(pca_pd, aes(x = Species, y = value, colour = Species)) +
+  theme_grey(base_size = 8) +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
+  facet_wrap(~ component, nrow = 1) +
+  xlab(NULL) + ylab("Value") + ggtitle("PCA on Cali phenotyping") +
+  scale_color_manual(values = pd[c(1, rep(2, 3), 3, 4)],
+                     guide = FALSE) +
+  geom_point(position = position_jitter(width = 0.4),
+             alpha = 0.8,
+             shape = 16) +
+  geom_boxplot(colour = alpha("black", 0.5),
+               fill = NA,
+               outlier.colour = NA)
+pcp
+
+# plot loadings
+lp <- ggplot(loadings_pd, aes(x = phenotype,
+                        yend = value,
+                        colour = phenotype,
+                        xend = phenotype)) +
+  theme_grey(base_size = 8) +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
+  xlab(NULL) + ylab("Loading") +
+  scale_color_brewer(palette = "Set1", guide = FALSE) +
+  facet_wrap(~ component, nrow = 1) +
+  geom_segment(y = 0,
+               size = 2,
+               arrow = arrow(angle = 15, length = unit(0.1, "in")))
+lp
+
+# draw plots
+layout_matrix <- matrix(
+  1:2,
+  nrow = 2)
+
+grobs <- arrangeGrob(pcp,
+                     lp,
+                     layout_matrix = layout_matrix)
+
+# save output
+cairo_pdf("test/pc1-4.pdf",
+          width = convertUnit(unit(180, "mm"), "in", valueOnly = TRUE),
+          height = convertUnit(unit(180, "mm"), "in", valueOnly = TRUE))
+grid.newpage()
+grid.draw(grobs)
+dev.off()
