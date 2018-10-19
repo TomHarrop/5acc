1d964288182a32f5a989f3bafa6d156cc2e6e524
Tom Harrop <twharrop@gmail.com>
2018-10-19 15:58:56 +1300

    font

--- src/figures/figure_s7.R
+++ src/figures/figure_s7.R
@@ -21,9 +21,9 @@ sample_dists_wide <- data.table(dist_matrix,   keep.rownames = TRUE)
 
 # long dt
 dist_dt <- melt(sample_dists_wide,
-     id.vars = "rn",
-     variable.name = "l2",
-     value.name = "Distance")
+                id.vars = "rn",
+                variable.name = "l2",
+                value.name = "Distance")
 setnames(dist_dt, "rn", "l1")
 dist_dt <- unique(dist_dt, by = c("l1", "l2"))
 
@@ -36,8 +36,11 @@ setorder(dist_dt, l1, l2)
 
 # plot
 gp <- ggplot(dist_dt, aes(x = l1, y = l2, fill = Distance)) +
-  theme_minimal() +
-  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5)) +
+  theme_minimal(base_size = 8, base_family = "Helvetica") +
+  theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5),
+        axis.ticks.length = unit(0, "mm")) +
+  scale_x_discrete(expand = c(0,0)) +
+  scale_y_discrete(expand = c(0,0)) +
   xlab(NULL) + ylab(NULL) +
   scale_fill_viridis_c() +
   geom_raster()
